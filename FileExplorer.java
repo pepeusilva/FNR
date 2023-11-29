eacbace3778dbec2b36f1143d7f185e78f9551f0
Pedro Augusto <111787739+pepeusilva@users.noreply.github.com>
2023-11-29 19:34:56 -0300

    File Explorer

---
+++ FileExplorer.java
@@ -0,0 +1,100 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileExplorer {
+
+    private File currentDirectory;
+
+    public FileExplorer() {
+        this.currentDirectory = new File(".");
+    }
+
+    public void pwd() {
+        System.out.println(currentDirectory.getAbsolutePath());
+    }
+
+    public void ls() {
+        for (File file : currentDirectory.listFiles()) {
+            System.out.println(file.getName());
+        }
+    }
+
+    public void cd(String path) throws IOException {
+        File newDirectory = new File(path);
+        if (!newDirectory.exists()) {
+            throw new IOException("Diretório não existe");
+        }
+        currentDirectory = newDirectory;
+    }
+
+    public void mkdir(String name) throws IOException {
+        File newDirectory = new File(currentDirectory, name);
+        if (newDirectory.exists()) {
+            throw new IOException("Diretório já existe");
+        }
+        newDirectory.mkdir();
+    }
+
+    public void rm(String name) throws IOException {
+        File file = new File(currentDirectory, name);
+        if (!file.exists()) {
+            throw new IOException("Arquivo não existe");
+        }
+        if (file.isDirectory()) {
+            rmdir(name);
+        } else {
+            file.delete();
+        }
+    }
+
+    private void rmdir(String name) throws IOException {
+        File directory = new File(currentDirectory, name);
+        if (!directory.isDirectory()) {
+            throw new IOException("O caminho especificado não é um diretório");
+        }
+        for (File file : directory.listFiles()) {
+            if (file.isDirectory()) {
+                rmdir(file.getName());
+            } else {
+                file.delete();
+            }
+        }
+        directory.delete();
+    }
+
+    public static void main(String[] args) throws IOException {
+        FileExplorer explorer = new FileExplorer();
+        while (true) {
+            System.out.print(">>> ");
+            String command = System.console().readLine();
+            String[] tokens = command.split(" ");
+            switch (tokens[0]) {
+                case "pwd":
+                    explorer.pwd();
+                    break;
+                case "ls":
+                    explorer.ls();
+                    break;
+                case "cd":
+                    explorer.cd(tokens[1]);
+                    break;
+                case "mkdir":
+                    explorer.mkdir(tokens[1]);
+                    break;
+                case "rm":
+                    explorer.rm(tokens[1]);
+                    break;
+                default:
+                    System.out.println("Comando não reconhecido");
+            }
+        }
+    }
+}
